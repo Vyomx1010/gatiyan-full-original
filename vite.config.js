9a99375b32ee1bd07e45ff56f5d69378cb1a92da
Navneet Vishwakarma <rajvl132011@gmail.com>
2025-03-08 21:49:46 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,6 @@ export default defineConfig({
       '/rides': 'http://localhost:3000',
       '/captains': 'http://localhost:3000',
       '/users': 'http://localhost:3000',
-      '/socket.io': {
-        target:'http://127.0.0.1:3000',
-        ws: true,
-      },
     },
   },
 });
