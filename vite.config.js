37a83d2697dc6cada49fe4fa70df9b0a2313599e
Navneet Vishwakarma <rajvl132011@gmail.com>
2025-03-18 23:30:33 +0530

    removing the proxy

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,13 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'https://gatiyan-full-original.vercel.app',
+      '/users': 'https://gatiyan-full-original.vercel.app',
+      '/captains': 'https://gatiyan-full-original.vercel.app',
+      '/maps': 'https://gatiyan-full-original.vercel.app', 
+      '/rides': 'https://gatiyan-full-original.vercel.app', 
+      '/payments': 'https://gatiyan-full-original.vercel.app',
+      '/admin-hubhaimere-sepanga-matlena': 'https://gatiyan-full-original.vercel.app', 
+      '/contact': 'https://gatiyan-full-original.vercel.app', 
     },
   },
 });
