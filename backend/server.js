8a0dc42f7e74e682be376b5e71e0ddb8bc06cf0f
Navneet Vishwakarma <rajvl132011@gmail.com>
2025-03-23 14:28:33 +0530

    remove vercel speed insight

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,10 @@
 const http = require('http');
 const app = require('./app');
-const { injectSpeedInsightsMiddleware } = require('@vercel/speed-insights/middleware'); // Use the middleware
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-// Inject the middleware for automatic tracking
-injectSpeedInsightsMiddleware(app);
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
